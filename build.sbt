55f62a3cf5555dbd4c9cce2ac3ebd398454a1b93
Matt Farmer <matt@frmr.me>
2014-04-24 10:38:45 -0400

    Add Mandrill implementation.

---
+++ build.sbt
@@ -0,0 +1,19 @@
+name := "mandrill-scala"
+
+version := "0.1.0-SNAPSHOT"
+
+organization := "com.hacklanta"
+
+scalaVersion := "2.10.4"
+
+libraryDependencies ++= {
+  val liftVersion = "2.6-M3"
+  Seq(
+    "net.liftweb"             %% "lift-common"        % liftVersion,
+    "net.liftweb"             %% "lift-util"          % liftVersion,
+    "net.liftweb"             %% "lift-json"          % liftVersion,
+    "net.databinder.dispatch" %% "dispatch-core"      % "0.11.0",
+    "net.databinder.dispatch" %% "dispatch-lift-json" % "0.11.0",
+    "org.scalatest"           %% "scalatest"          % "2.0" % "test"
+  )
+}
---
+++ src/main/scala/Mandrill.scala
@@ -0,0 +1,74 @@
+package com.hacklanta.mandrill
+
+import net.liftweb._
+  import common._
+  import util.Helpers._
+  import json._
+    import JsonDSL._
+    import Extraction._
+  import util._
+
+import dispatch._, Defaults._
+import com.ning.http.client.{Request, RequestBuilder, Response}
+
+case class MandrillTo(email: String, name: Option[String] = None, `type`: String = "to")
+case class MandrillMessage(
+  subject: String,
+  from_email: String,
+  to: List[MandrillTo],
+  from_name: Option[String] = None,
+  html: Option[String] = None,
+  text: Option[String] = None
+)
+
+trait MandrillApiCall {
+  def uri(requestBuilder: dispatch.Req): dispatch.Req
+}
+case class SendMandrillMessage(message: MandrillMessage, async: Boolean = false) extends MandrillApiCall {
+  def uri(requestBuilder: dispatch.Req) = requestBuilder / "messages" / "send.json"
+}
+case class SendTemplateMandrillMessage(message: MandrillMessage, template_name: String, template_content: List[Map[String, String]], async: Boolean = false) extends MandrillApiCall {
+  def uri(requestBuilder: dispatch.Req) = requestBuilder / "messages" / "send-template.json"
+}
+
+object Mandrill extends Loggable {
+  implicit val formats = DefaultFormats
+
+  private val key = Props.get("mandrill.apiKey") openOr ""
+  private val endpointHost = Props.get("mandrill.endpointHost") openOr "mandrillapp.com"
+  private val endpointVersion = Props.get("mandrill.endpointVersion") openOr "1.0"
+
+  case class CodeResponse(code: Int)
+  protected object AsCodeResponse extends (Response => CodeResponse) {
+    def apply(r:Response) = {
+      CodeResponse(
+        r.getStatusCode()
+      )
+    }
+  }
+
+  case class MandrillResponse(status: String)
+
+  def run(apiCall: MandrillApiCall) = {
+    val postJson = decompose(apiCall) match {
+      case obj:JObject =>
+        obj ~
+        ("key" -> key)
+
+      case _ =>
+        JObject(Nil)
+    }
+
+    val requestBody = compact(render(postJson))
+    val request = (apiCall.uri(host(endpointHost) / "api" / endpointVersion)).secure <<
+      requestBody
+
+    val response = Http(request > AsCodeResponse).either
+
+    response() match {
+      case Right(CodeResponse(200)) => // All good.
+      case Right(CodeResponse(code)) => logger.error("Mandrill returned code: " + code)
+      case Left(dispatchError) => logger.error("Dispatch error: " + dispatchError)
+    }
+  }
+}
